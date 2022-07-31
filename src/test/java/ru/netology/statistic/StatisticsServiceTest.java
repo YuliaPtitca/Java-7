2273cdf6785b92249e8828023a861960783d8699
unknown <qa.yulia.ptitca@yandex.ru>
2022-07-31 22:21:23 +0300

    Create project

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,16 +17,4 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
-
-    @Test
-    void findMax2() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
 }
